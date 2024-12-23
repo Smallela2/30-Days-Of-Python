24f995209604fd88a6ee4286c0e931aa3a625bf8
Smallela2 <swathimallela26@gmail.com>
2024-12-23 11:27:10 +0530

    Update sets.py

--- 07_Day_Sets/sets.py
+++ 07_Day_Sets/sets.py
@@ -1,1 +1,11 @@
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+print("length", len(it_companies))
+it_companies.add("Twitter")
+print(it_companies)
+companies = ("Capgemini", "Hcl")
+it_companies.update(companies)
+print(it_companies)
+it_companies.discard("Apple")
+print("after remove:", it_companies)
+
 
