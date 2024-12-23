678a45b06df2ab549532ce2dcc256f72e2623fc5
Smallela2 <swathimallela26@gmail.com>
2024-12-23 11:45:52 +0530

    Update sets.py

--- 07_Day_Sets/sets.py
+++ 07_Day_Sets/sets.py
@@ -35,3 +35,9 @@ elif len(age) < len(my_set):
     print("my_set is bigger")
 else:
     print("Both are Equal")
+sentence = "I am a teacher and I love to inspire and teach people."
+# Split the sentence into words and convert to a set to get unique words
+words = sentence.split()  # Splits the sentence into a list of words
+unique_words = set(words)  # Convert the list to a set to remove duplicates
+print("Unique words:", unique_words)
+print("Number of unique words:", len(unique_words))
