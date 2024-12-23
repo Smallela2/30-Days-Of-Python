666966666ff975fdec4c08ddfad09b66cd170277
Smallela2 <swathimallela26@gmail.com>
2024-12-23 11:36:19 +0530

    Update sets.py

--- 07_Day_Sets/sets.py
+++ 07_Day_Sets/sets.py
@@ -1,3 +1,4 @@
+#Exercise level 1
 it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
 print("length", len(it_companies))
 it_companies.add("Twitter")
@@ -9,3 +10,16 @@ it_companies.discard("Apple")
 print("after remove:", it_companies)
 
 
+#Exercise level 2
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+C = A.union(B)
+print(C)
+D =A.intersection(B)
+print(D)
+print(A.issubset(B))
+print(A.isdisjoint(B))
+print(A.union(B) & B.union(A))
+print(A.symmetric_difference(B))
+del A
+del B
