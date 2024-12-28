0ed69f04b8b589749f2b6205d4c16e3519c743fa
Smallela2 <swathimallela26@gmail.com>
2024-12-28 17:04:18 +0530

    Update functions.py

--- 11_Day_Functions/functions.py
+++ 11_Day_Functions/functions.py
@@ -2,3 +2,9 @@ def add_two_numbers(num1,num2):
     sum = num1+num2
     return sum
 print(add_two_numbers(2,3))
+
+import math
+def area_of_a_circle(r):
+    circle = math.pi *r*r
+    return circle
+print(area_of_a_circle(1))
