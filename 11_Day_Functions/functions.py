49a78b6fe033ca668e6892f924b081ca2e3cdbea
Smallela2 <swathimallela26@gmail.com>
2024-12-28 17:10:48 +0530

    Update functions.py

--- 11_Day_Functions/functions.py
+++ 11_Day_Functions/functions.py
@@ -8,3 +8,15 @@ def area_of_a_circle(r):
     circle = math.pi *r*r
     return circle
 print(area_of_a_circle(1))
+
+def add_all_nums(*args):
+    total = 0
+    for num in args:
+        if not isinstance(num, (int, float)):  # Check if the argument is a number
+            return "All arguments must be numbers!"
+        total += num
+    return total
+
+print(add_all_nums(2, 3, 4, 5))  # Valid input
+print(add_all_nums(2, '3', 4, 5))  # Invalid input (string in arguments)
+print(type(add_all_nums()))  # Type of the returned result
