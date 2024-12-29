b0df9077298f7bae641bdc3697874b9537ddae0f
Smallela2 <swathimallela26@gmail.com>
2024-12-29 21:23:38 +0530

    Update modules.py

--- 12_Day_Modules/modules.py
+++ 12_Day_Modules/modules.py
@@ -1,1 +1,10 @@
+import random
+import string
+
+def random_user_id():
+    # Generate a random string of 6 characters (letters and digits)
+    user_id = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+    return user_id
+
+print(random_user_id())  # Example output: '1ee33d'
 
