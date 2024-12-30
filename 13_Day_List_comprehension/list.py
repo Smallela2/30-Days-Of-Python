edf8ae35bca50427b6518b7248dfedd7e098b3a0
Smallela2 <swathimallela26@gmail.com>
2024-12-30 17:46:41 +0530

    Update list.py

--- 13_Day_List_comprehension/list.py
+++ 13_Day_List_comprehension/list.py
@@ -3,3 +3,8 @@ lst = [i for i in numbers if i<=0]
 print(lst)
 
 
+list_of_lists =[[[1, 2, 3]], [[4, 5, 6]], [[7, 8, 9]]]
+flatten_list = [number for row in list_of_lists  for number in row]
+lst = [number for row in flatten_list  for number in row]
+print(lst)
+
