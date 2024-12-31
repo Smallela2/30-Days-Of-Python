d619e59b0ce31f8127c33e5290e432dc9b22107d
Smallela2 <swathimallela26@gmail.com>
2024-12-31 12:42:58 +0530

    Update higher_order_functions.py

--- 14_Day_Higher_order_functions/higher_order_functions.py
+++ 14_Day_Higher_order_functions/higher_order_functions.py
@@ -18,3 +18,8 @@ print(countries_uppercase)
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 square_numbers = list(map(lambda x: x*x,numbers))
 print(square_numbers)
+
+
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+upper_names = list(map(lambda name: name.upper(),names))
+print(upper_names)
